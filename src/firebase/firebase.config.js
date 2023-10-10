28fe99f5e1289cdb53080709c9ffd12631f619da
SaddamHossain07 <contact.saddam3@gmail.com>
2023-10-10 15:30:43 +0600

    secure firebase private information

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA-QL4ZBTvnUN56dbGHgJv3qpsnjpCF_GE",
-    authDomain: "corporate-event-manageme-5e5db.firebaseapp.com",
-    projectId: "corporate-event-manageme-5e5db",
-    storageBucket: "corporate-event-manageme-5e5db.appspot.com",
-    messagingSenderId: "725715926518",
-    appId: "1:725715926518:web:80bf60b187f583a68b5bd7"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
